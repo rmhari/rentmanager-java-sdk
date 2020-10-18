311cbe80a2106aedcaa4367833720b0e2efb7abb
haripriya <rm.haripriya@gmail.com>
2020-10-18 10:19:01 +0530

    Move to 11

--- src/test/java/com/rentmanager/client/PaymentsTest.java
+++ src/test/java/com/rentmanager/client/PaymentsTest.java
@@ -1,9 +1,6 @@
 package com.rentmanager.client;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
@@ -11,20 +8,20 @@ import java.util.Optional;
 import com.rentmanager.client.model.Payment;
 
 import com.rentmanager.exception.RentManagerException;
-import org.junit.jupiter.api.Test;
+
 
 public class PaymentsTest {
 
-    @Test
-    void testRentManager() throws RentManagerException {
+    public static void main(String[] args) throws RentManagerException {
         RentManager rentManager = new RentManager.RentManagerBuilder().url("https://epc.api.rentmanager.com").userName("heman").password("Eppo1234").build();
         Optional<List<Payment>> result = rentManager.newRequestBuilder(Payment.class).getEntities(null,
                 List.of("Allocations", "Allocations.Charge", "Allocations.Charge.ChargeType", "EpayTransactionInformationDetail.EpayTransactionInformation", "EpayTransactionInformationDetail", "Tenant", "Tenant.Addresses"), Collections.singletonList("LastFirst"), null, 20000, null);
         System.out.println(result);
-        assertNotNull(result);
+
         result.ifPresent(entities -> {
-            assertTrue(entities.size() > 0);
+            System.out.println(entities.size());
         });
     }
+
 
 }
